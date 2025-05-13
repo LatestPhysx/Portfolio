713517ec142d732851f529baa9c3b65cc095e18a
Omar Bahloul <omarbahloul2004@gmail.com>
2025-05-13 21:20:13 +0100

    V0.0.3

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import '../App.css'
-import emailLogo from '../assets/email.svg'
-import downloadLogo from '../assets/download.svg'
+import  EmailIcon from '../assets/email.svg?react'
+import  DownloadIcon from '../assets/download.svg?react'
 
 const Navbar = () => {
-  return (
+  return (<>
     <nav className='w-full navbar flex justify-between items-center h-fit p-8'>
-        <span className='flex'><img src={emailLogo} className='mr-3' alt="" /> omarbahloul2004@gmail.com</span>
+        <span className='flex items-center'><EmailIcon className='mr-1.5'/> omarbahloul2004@gmail.com</span>
         <div className='flex capitalize items-center content-center gap-5'>
             <span>about</span>
             <span>education</span>
             <span>projects</span>
             <span>contact</span>
-            <button className='flex bg-black rounded text-white p-2.5'>Resume <img src={downloadLogo} className='ml-2' alt="" /></button>
+            <button className='flex bg-black dark:text-black dark:bg-white rounded text-white items-center p-2.5'>Resume <DownloadIcon className='ml-1.5' /></button>
         </div>
     </nav>
-  )
+    </>)
 }
 
 export default Navbar

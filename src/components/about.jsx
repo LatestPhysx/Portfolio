65570c2881634042cc68c4b56300c5a91188341c
Omar Bahloul <omarbahloul2004@gmail.com>
2025-05-15 21:21:08 +0100

    V0.0.6

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import '../App.css'
 import FingerIcon from '../assets/finger.svg?react'
+import DownloadIcon from '../assets/downloadVariant.svg?react'
+import Perso from '../assets/perso.png'
 
 const About = () => {
-  return (
-    <section className='about mt-10 w-full h-fit p-5 flex flex-col text-center justify-center'>
-        <span className='mb-5 flex justify-center'>Hi! I’m Omar<FingerIcon className='ml-1 sm:ml-2.5' /></span>
-        <span className='mb-5 font-bold text-xl'>Full-Stack Devoloper Based<br /> In Casablanca, Morocco</span>
-        <span className='capitalize text-neutral-600'>I specialize in creating Full-Stack Websites<br /> With The Latest Technologies<br /> Visually good looking ui's</span>
+  return (<>
+    <section className='about my-10 w-full h-fit p-5 flex flex-col text-center justify-center'>
+      <span className='mb-5 flex justify-center'><img className='w-50 h-auto' src={Perso} alt="" /></span>
+      <span className='mb-5 flex justify-center'>Hi! I’m Omar<FingerIcon className='ml-1' /></span>
+      <span className='mb-5 font-bold text-xl'>Full-Stack Devoloper Based<br /> In Casablanca, Morocco</span>
+      <span className='capitalize text-neutral-600 dark:text-neutral-300'>I specialize in creating Full-Stack Websites<br /> With The Latest Technologies<br /> Visually good looking ui's</span>
     </section>
-  )
+    <div className='w-full p-5 grid grid-cols-1 sm:grid-cols-2 gap-5 md:grid-cols-2 h-fit'>
+      <button className='p-4 text-lg m-auto md:m-0 md:ml-auto capitalize bg-black dark:bg-white content-center w-35 h-fit text-white dark:text-black rounded-3xl'>hire me</button>
+      <button className='p-4 text-lg m-auto md:m-0 md:mr-auto flex capitalize w-fit h-fit rounded-2xl'>download CV<DownloadIcon className='ml-2.5' /></button>
+    </div>
+  </>)
 }
 
-export default About
+export default About  
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,7 +10,7 @@ import ProjectIcon from '../assets/projects.svg?react'
 import ContactIcon from '../assets/contact.svg?react'
 
 const Navbar = () => {
-    const [sideMenu, showSideMenu] = useState(true);
+    const [sideMenu, showSideMenu] = useState(false);
     let sideMenuStyle;
     if (sideMenu) {
         sideMenuStyle = 'flex flex-col gap-5 capitalize transition-all duration-700 ease-in lg:hidden fixed w-1/2 p-3 justify-center text-white h-full top-0 right-0 bg-black'
@@ -31,12 +31,12 @@ const Navbar = () => {
                 <button className='flex bg-black dark:text-black dark:bg-white rounded text-white items-center p-2.5'>Resume <DownloadIcon className='ml-1.5' /></button>
             </div>
             <div className={sideMenuStyle}>
-                <button onClick={() => showSideMenu((prevState) => !prevState)} className='absolute hover:cursor-pointer top-0 right-0 p-5'><CloseIcon className='ml-1.5' /></button>
+                <button onClick={() => showSideMenu((prevState) => !prevState)} className='absolute hover:cursor-pointer top-0 right-0 p-2 sm:p-5'><CloseIcon className='ml-1.5' /></button>
                 <span className='flex items-center py-4 px-2 sm:p-5 hover:bg-gray-200 duration-700 hover:text-black rounded'><AboutIcon className='mr-2.5' />about</span>
                 <span className='flex items-center py-4 px-2 sm:p-5 hover:bg-gray-200 duration-700 hover:text-black rounded'><EducationtIcon className='mr-2.5' />education</span>
                 <span className='flex items-center py-4 px-2 sm:p-5 hover:bg-gray-200 duration-700 hover:text-black rounded'><ProjectIcon className='mr-2.5' />projects</span>
                 <span className='flex items-center py-4 px-2 sm:p-5 hover:bg-gray-200 duration-700 hover:text-black rounded'><ContactIcon className='mr-2.5' />contact</span>
-                <button className='flex bg-white hover:bg-gray-300 duration-700 text-black w-fit rounded items-center p-2 my-4 mx-2 sm:m-5'>Resume <DownloadIcon className='ml-1.5 w-3 h-3' /></button>
+                <button className='flex hover:cursor-pointer bg-white hover:bg-gray-300 duration-700 text-black w-fit rounded items-center p-2 my-4 mx-2 sm:m-5'>Resume <DownloadIcon className='ml-1.5 w-3 h-3' /></button>
             </div>
         </nav>
     </>)
